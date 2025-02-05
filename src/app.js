9585c3f34f1c1cdf0c2b66f7fb30416c5e1208d6
sruthi <punnanasruthi@gmail.com>
2025-02-05 18:33:09 +0530

    http methods

--- src/app.js
+++ src/app.js
@@ -1,12 +1,40 @@
 const express = require('express')
 const app = express()
-// app.use((req, res)=>{
+
+// app.use("/",(req, res)=>{
 //     res.send("Handles all request handlers")
 // })
+
+// http methods
+
+app.get("/user", (req,res)=>{
+  res.send("Get method is working")
+})
+
+app.post("/user", (req,res)=>{
+  res.send("Post method is working")
+})
+
+app.delete("/user", (req,res)=>{
+  res.send("delete method is working")
+})
+
+app.patch("/user", (req,res)=>{
+  res.send("patch method is working")
+})
+
+// 
+
+// any method, same output for route if we use "use"
+
+app.use('/test/2',(req,res)=>{
+  res.send("/test route 2 is working")
+})
 
 app.use('/test',(req,res)=>{
   res.send("/test route is working")
 })
+
 
 app.use('/hello',(req,res)=>{
     res.send("/hello is working")
